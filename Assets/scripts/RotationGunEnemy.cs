57341370586fcd9f14f0c26b833b88159b56c004
Grifst <kuzia91grifst@gmail.com>
2021-02-10 04:54:44 +1000

    Shoting
    Враг теперь стреляет так же сделано универсальная настройка боеприпасов

--- Assets/scripts/RotationGunEnemy.cs
+++ Assets/scripts/RotationGunEnemy.cs
@@ -5,9 +5,9 @@ using UnityEngine;
     {
         [SerializeField] private Shoting _shoting;
         [SerializeField] Transform target;
-       
-       
-        
+        public float angle = 60;
+        private float timer = 0f;
+        public float reload = 3;
     private void Start()
         {
             target = FindObjectOfType<RotationGunPlayer>().transform;
@@ -24,9 +24,21 @@ using UnityEngine;
 
         private void GetAngleAttack()
         {
-            //логика наведения
-            //если угол = угол атаки то 
-            //shoting.Shot();
+        //логика наведения
+        //если угол = угол атаки то 
+        //
+
+        if (Vector3.Angle(transform.forward, target.position - transform.position) <= angle)
+        {
+            
+            timer += Time.deltaTime;
+            if (timer > reload)
+            {
+                _shoting.Shoot();
+                timer = 0;
+            }
         }
+
+    }
 
     }
