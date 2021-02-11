8c105f9f924dfdf8c6fe96fac753e2a841cccede
Grifst <kuzia91grifst@gmail.com>
2021-02-11 21:18:48 +1000

    New script
    ХП броня и урон

---
+++ Assets/scripts/HelthControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelthControl : MonoBehaviour
+{
+    [Header("Здоровье и Щиты")]
+    [SerializeField] private float _maxHelthPoint = 100;
+    [SerializeField] private float _currentHP;
+    [SerializeField] private float _maxShieldPoint;
+    [SerializeField] private float _currentSP;
+
+
+    private void Start()
+    {
+        _maxShieldPoint = _currentSP;
+        _maxHelthPoint = _currentHP;
+
+    }
+    void Update()
+    {
+
+    }
+    
+   public void TakeDamage(float damage)
+    {
+          _currentSP -= damage;
+
+        if(_currentSP <= 0)
+        {
+              _currentHP -= damage;
+
+            if(_currentHP <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
+
+}
